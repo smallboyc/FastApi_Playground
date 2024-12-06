b5a9228a2059cf92f2b75a48fbdb676cf13d5fcd
smallboyc <maxencedup44@gmail.com>
2024-12-07 00:25:00 +0100

    ✨ Show images data

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+from typing import List
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+class Photo(BaseModel):
+    imageName: str
+    imageData: str
+
+
+@app.post("/process-image/")
+async def process_image(photos: List[Photo]):
+    for photo in photos:
+        print(photo.imageName)
+
+    return {"Image processing complete"}
